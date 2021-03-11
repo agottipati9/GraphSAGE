0cacb93638f53296669363a69d0bfe5c76f339d3
Aashish Gottipati <agot@node.agot-qv93650.powdersandbox.emulab.net>
2021-03-10 18:45:27 -0700

    example scripts

---
+++ Ubuntu_Docker_Install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt-cache policy docker-ce
+apt install -y docker-ce
+chmod 666 /var/run/docker.sock
+
+echo "Finished Docker Install"
+
+apt update
+apt install -y python3.6
+apt install -y python3-pip
+
+echo "Installed Python3"
+
