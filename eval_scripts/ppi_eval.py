86cfb210d15d3d4f06ec151175c9b91af81f2bed
aashish <u1174579@utah.edu>
2021-03-23 18:12:07 -0600

    capped to 8k examples

--- eval_scripts/ppi_eval.py
+++ eval_scripts/ppi_eval.py
@@ -11,7 +11,7 @@ Run this script after running unsupervised training.
 Baseline of using features-only can be run by setting data_dir as 'feat'
 Example:
   python eval_scripts/ppi_eval.py ./example_data/toy unsup-example_data/graphsage_mean_small_0.000010 test no > results.txt
-  python eval_scripts/ppi_eval.py ./example_data/toy unsup-example_data/graphsage_mean_small_0.000010 test yes > results.txt
+  python eval_scripts/ppi_eval.py ./example_data/toy unsup-example_data/graphsage_mean_small_0.000010 test yes > results_tda.txt
 '''
 
 def run_regression(train_embeds, train_labels, test_embeds, test_labels):
@@ -26,6 +26,9 @@ def run_regression(train_embeds, train_labels, test_embeds, test_labels):
     log.fit(train_embeds, train_labels)
     f1 = 0
     for i in range(test_labels.shape[1]):
+        print("TARGET ", test_labels[:,i])
+        print(test_embeds.max())
+        print("PRED ", log.predict(test_embeds)[:,i])
         print("F1 score", f1_score(test_labels[:,i], log.predict(test_embeds)[:,i], average="micro"))
     for i in range(test_labels.shape[1]):
         print("Random baseline F1 score", f1_score(test_labels[:,i], dummy.predict(test_embeds)[:,i], average="micro"))
@@ -85,26 +88,23 @@ if __name__ == '__main__':
         run_regression(train_feats, train_labels, test_feats, test_labels)
     elif use_tda == 'yes':
         embeds = np.genfromtxt('tda_embeds.csv', delimiter=',')
-        id_map = {}
-        with open(data_dir + "/val.txt") as fp:
-            for i, line in enumerate(fp):
-                id_map[int(line.strip())] = i
-        train_embeds = embeds[[id_map[id] for id in train_ids]] 
-        test_embeds = embeds[[id_map[id] for id in test_ids]] 
+        train_embeds = embeds[:7000] # using only 8k for sake of computation
+        test_embeds = embeds[7000:] # using only 8k for sake of computation
         print("Running regression..")
-        run_regression(train_embeds, train_labels, test_embeds, test_labels)
+        run_regression(train_embeds, train_labels[:7000], test_embeds, test_labels[:1000])
     else:
         embeds = np.load(data_dir + "/val.npy")
         id_map = {}
         with open(data_dir + "/val.txt") as fp:
             for i, line in enumerate(fp):
                 id_map[int(line.strip())] = i
-        train_embeds = embeds[[id_map[id] for id in train_ids]] 
-        test_embeds = embeds[[id_map[id] for id in test_ids]] 
+        train_embeds = embeds[[id_map[id] for id in train_ids]][:7000]
+        test_embeds = embeds[[id_map[id] for id in test_ids]][:1000]
         
         # Save for TDA
-        df = pd.DataFrame(data=embeds.astype(float))
+        temp = np.concatenate((train_embeds[:7000], test_embeds[:1000]))
+        df = pd.DataFrame(data=temp.astype(float))
         df.to_csv('point_cloud_embeds.csv', sep=',', header=False, index=False)
 
         print("Running regression..")
-        run_regression(train_embeds, train_labels, test_embeds, test_labels)
+        run_regression(train_embeds, train_labels[:7000], test_embeds, test_labels[:1000])
